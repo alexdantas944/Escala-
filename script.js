79f57d612f22d38cbdbf72b3d3ad363b2dd08919
alexdantas944 <dantassilva017@gmail.com>
2025-05-29 13:26:16 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function buscarCep() {
+    let cep = document.getElementById("cep").value;
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                document.getElementById("resultadoCep").innerText = "CEP não encontrado!";
+            } else {
+                document.getElementById("resultadoCep").innerText = `Endereço: ${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`;
+            }
+        })
+        .catch(error => console.error("Erro ao buscar CEP:", error));
+}
+
+function buscarLogradouro() {
+    let logradouro = document.getElementById("logradouro").value;
+    let uf = "CE"; // Estado fixo (pode ser modificado para tornar mais dinâmico)
+    let cidade = "Fortaleza"; // Cidade fixa (também pode ser modificada)
+    
+    fetch(`https://viacep.com.br/ws/${uf}/${cidade}/${logradouro}/json/`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length === 0) {
+                document.getElementById("resultadoLogradouro").innerText = "Endereço não encontrado!";
+            } else {
+                let cepEncontrado = data[0].cep;
+                document.getElementById("resultadoLogradouro").innerText = `CEP encontrado: ${cepEncontrado}`;
+            }
+        })
+        .catch(error => console.error("Erro ao buscar Logradouro:", error));
+}
