2b0d2562b2e99647ab8a04e619987bb99fc5b103
alexdantas944 <dantassilva017@gmail.com>
2025-05-29 14:46:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,7 @@ self.addEventListener('install', event => {
                 '/index.html',
                 '/style.css',
                 '/script.js',
+                '/manifest.json',
                 '/icon.png'
             ]);
         })
@@ -15,7 +16,12 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).then(networkResponse => {
+                return caches.open('cep-app-cache').then(cache => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
         })
     );
 });
