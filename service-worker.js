7eda849a3f18080d072b774909800b18d3cb7525
alexdantas944 <dantassilva017@gmail.com>
2025-05-30 14:25:41 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-self.addEventListener("install", event => {
+self.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open("escala-cache").then(cache => {
-            return cache.addAll(["/", "/index.html", "/styles.css", "/script.js"]);
+        caches.open("folga-cache").then(function (cache) {
+            return cache.addAll(["index.html", "styles.css", "script.js"]);
         })
     );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", function (event) {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
         })
     );
